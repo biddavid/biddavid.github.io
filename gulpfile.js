a43275704341da958839d8c7f922cc092ff0f042
AppleUser <appleuser@ITANY-IMAC-106.local>
2017-07-03 18:06:06 +0800

    111111

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+/*
+	gulpfile.js主配置文件，用于定义任务
+	此处的代码由Node执行
+
+*/
+
+'use strict';
+//加载模块
+var gulp=require('gulp');
+var less=require('gulp-less');
+// var cssnano=require('gulp-cssnano');
+var cssmin=require('gulp-cssmin');
+var concat=require('gulp-concat');
+var uglify=require('gulp-uglify');
+var imagemin=require('gulp-imagemin');
+var clean=require('gulp-clean');
+var browserSync=require('browser-sync').create();
+
+//定义一个简单的任务
+gulp.task('hello',function(){
+	console.log('hello World');
+});
+
+//html复制文件的任务
+gulp.task('html',function(){
+	gulp.src('src/**/*.html')//读取文件
+	.pipe(gulp.dest('dist/'));//通过管道再次操作，写入到目标位置
+});
+
+//less编译和压缩到任务
+gulp.task('less',function(){
+	gulp.src('src/less/*.less')
+	.pipe(less())//编译
+	// .pipe(cssnano())//压缩
+	.pipe(cssmin())
+	.pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('js',function(){
+	gulp.src('src/js/*.js')
+	.pipe(concat('all.js'))
+	//.pipe(uglify())
+	.pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('image',function(){
+	gulp.src('src/images/*')
+	.pipe(imagemin())
+	.pipe(gulp.dest('dist/images/'));
+});
+
+gulp.task('clean',function(){
+	gulp.src('dist/')
+	.pipe(clean())
+});
+
+gulp.task('dist',['html','less','js','image']);
+
+gulp.task('watch',function(){
+	gulp.watch('src/images/*',['image']);
+	gulp.watch('src/js/*.js',['js']);
+	gulp.watch('src/less/*.less',['less']);
+	gulp.watch('src/**/*.html',['html']);
+});
+
+gulp.task('serve',['js','less','html','image','watch'],function(){
+	browserSync.init({
+		server:{
+			baseDir:'./dist'
+		},
+		port:2017
+	});
+});
---
+++ src/js/1.js
@@ -0,0 +1,4 @@
+function(username,age){
+	console.log(username);
+	console.log(age);
+}
